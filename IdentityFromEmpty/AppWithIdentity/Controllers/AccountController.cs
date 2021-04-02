2f332a54d8b631fde208d04139b582102da24b02
DESKTOP-B7036PK\tatsuteb <tatsuteb@gmail.com>
2021-04-03 08:39:50 +0900

    新規登録画面を作成 新規登録機能を実装

--- IdentityFromEmpty/AppWithIdentity/Controllers/AccountController.cs
+++ IdentityFromEmpty/AppWithIdentity/Controllers/AccountController.cs
@@ -21,6 +21,77 @@ namespace AppWithIdentity.Controllers
             _userManager = userManager;
         }
 
+        #region 新規登録
+
+        public IActionResult Register([FromQuery]string returnUrl)
+        {
+            ViewData["returnUrl"] = returnUrl ?? Url.Action("Index", "Home");
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register([FromForm]Register model, string returnUrl)
+        {
+            try
+            {
+                returnUrl = string.IsNullOrWhiteSpace(returnUrl) 
+                    ? Url.Action("Index", "Home") 
+                    : returnUrl;
+
+                if (!ModelState.IsValid)
+                {
+                    throw new Exception("不正な入力です。");
+                }
+
+                var user = new IdentityUser(model.UserName)
+                {
+                    Email = model.Email,
+                };
+                
+                var createUserResult = await _userManager.CreateAsync(
+                    user: user,
+                    password: model.Password);
+
+                if (!createUserResult.Succeeded)
+                {
+                    foreach (var identityError in createUserResult.Errors)
+                    {
+                        ModelState.AddModelError("CreateUserError", identityError.Description);
+                    }
+
+                    throw new Exception("新規登録に失敗しました。");
+                }
+
+                var signInResult = await _signInManager.PasswordSignInAsync(
+                    user: user,
+                    password: model.Password,
+                    isPersistent: false,
+                    lockoutOnFailure: false);
+
+                if (!signInResult.Succeeded)
+                {
+                    throw new Exception("ログインに失敗しました。");
+                }
+
+                return LocalRedirect(returnUrl);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                
+                ModelState.AddModelError("RegisterError", e.Message);
+                
+                ViewData["returnUrl"] = returnUrl;
+                model.Password = "";
+
+                return View(model);
+            }
+        }
+
+        #endregion
+
+
         #region ログイン
 
         public IActionResult Login([FromQuery]string returnUrl)
